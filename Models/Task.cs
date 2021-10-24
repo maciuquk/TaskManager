ea63b364c3dc6a31a68b2100236e95408b23e76d
maciuquk <maciejromik@gmai.com>
2021-10-24 14:32:46 +0200

    Add task CRUD

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace TaskManager.Models
     public class Task
     {
         public int ID { get; set; }
+        [Display(Name ="Nazwa")]
         public string TaskName { get; set; }
+        [Display(Name ="Data")]
         public DateTime Date { get; set; }
+        [Display(Name ="Czy zakończone?")]
         public bool IsDone { get; set; }
 
     }
